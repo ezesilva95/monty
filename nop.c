9a775d5572625e1121457d1119f6c3540708df98
ezesilva95 <4381@holbertonschool.com>
2022-05-12 05:53:08 -0700

    Task 5

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - doesn’t do anything.+
+ * @stack: a
+ * @line_number: location of instruction
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
