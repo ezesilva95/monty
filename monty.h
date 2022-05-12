7e3122eb7af8109b342662c0b07143948a5acd81
ezesilva95 <4381@holbertonschool.com>
2022-05-12 13:11:57 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -19,9 +19,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -34,8 +34,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number);
@@ -46,4 +46,5 @@ int get_function(char *tok, stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+int main(int argc, char *argv[]);
 #endif
