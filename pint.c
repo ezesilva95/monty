5fcf4f0338714c267e701d6c52caa76dddf944be
ezesilva95 <4381@holbertonschool.com>
2022-05-11 21:01:13 -0700

    Task 1

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/*
+ *pint - prints the value at the top of the stack
+ *@stack: a
+ *@line_number: location of the instruction
+*/
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = *stack;
+
+	if (*stack == NULL)
+	{
+		printf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", new_node->n);
+}
