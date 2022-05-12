1037e881fc0f2e83bb7e7223ec66f90f90fd3070
ezesilva95 <4381@holbertonschool.com>
2022-05-11 20:34:16 -0700

    Auxiliar functions

---
+++ aux_func.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/*
+ * is_number - check if char are numers
+ * @nmb: n
+ * Return: 0 if success -1 if not
+ */
+int is_number(char *nmb)
+{
+	int i = 0;
+	if (nmb[0] == '-')
+		i = 1;
+	while (nmb[i] != '\0')
+	{
+		if (nmb[i] < '0' || nmb[i] > '9')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
