a26694fc14a06c6d630457183d5de61d032b7f63
ezesilva95 <4381@holbertonschool.com>
2022-05-11 23:39:57 -0700

    Task 0

--- push-pall.c
+++ push-pall.c
@@ -51,8 +51,8 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void) line_number;
 	stack_t *node = *stack;
+	(void) line_number;
 	if (!stack || !*stack)
 		return;
 	while (node != NULL)
