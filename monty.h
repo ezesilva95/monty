e33e2706546dc595f8064b0f1b58c22dbfaf599c
ezesilva95 <4381@holbertonschool.com>
2022-05-11 23:33:39 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -1,3 +1,6 @@
+#ifndef MONTY_H
+#define MONTY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -38,4 +41,7 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+int is_number(char *nmb);
+int get_function(char *tok, stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 #endif
