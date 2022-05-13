7f7a91c7e054ac59e68b9670c3ef3cae9da06b61
ezesilva95 <4381@holbertonschool.com>
2022-05-12 17:05:21 -0700

    task 2

--- pop.c
+++ pop.c
@@ -6,16 +6,25 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp = *stack;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		free_dlistint(stack);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		*stack = (*stack)->next;
-		free((*stack)->prev);
-		(*stack)->prev = NULL;
+		if ((*stack)->next == NULL)
+		{
+			*stack = NULL;
+			free(temp);
+		}
+		else
+		{
+			*stack = (*stack)->next;
+			(*stack)->prev = NULL;
+			free(temp);
+		}
 	}
 }
