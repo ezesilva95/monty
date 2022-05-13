bbdb9872f204cf16ca9f2d0f922a3048ef3668e3
ezesilva95 <4381@holbertonschool.com>
2022-05-12 17:55:31 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -51,4 +51,5 @@ void sub(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *stack);
+void div(stack_t **stack, unsigned int line_number);
 #endif
