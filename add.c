d38f98733e33ff5c21a2dbcf122c7a036ea8a9d5
ezesilva95 <4381@holbertonschool.com>
2022-05-12 17:11:15 -0700

    task 4

--- add.c
+++ add.c
@@ -7,11 +7,12 @@
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
+	stack_t *temp2 = *stack;
 
 	if (*stack != NULL && (*stack)->next != NULL)
 	{
 		temp = (*stack)->next;
-		temp->n += (*stack)->n;
+		temp->n += temp2->n;
 		pop(stack, line_number);
 		*stack = temp;
 	}
